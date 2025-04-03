ccc8618d1d3d060cf645bd8d384c7a92ec0ccba4
v.efremov <v.efremov@cian.ru>
2025-04-03 19:11:17 +0700

    Add tests

---
+++ test_main.py
@@ -0,0 +1,33 @@
+from fastapi.testclient import TestClient
+
+from main import app
+
+client = TestClient(app)
+
+
+def test_get_offers():
+    response = client.get("/offers")
+    assert response.status_code == 200
+    assert response.json() == [
+        {
+            'id': 1,
+            'location': 'Томск, пер. Лесной 10',
+            'photos': ['url1', 'url2'],
+            'price': 100,
+            'rooms': 3
+        }
+    ]
+
+
+def test_get_offer():
+    response = client.get("/offers/1")
+    assert response.status_code == 200
+    assert response.json() == {
+        'id': 1,
+        'location': 'Томск, пер. Лесной 10',
+        'phone': '+7777777777',
+        'photos': ['url1', 'url2'],
+        'price': 100,
+        'rooms': 3,
+        'total_area': 100
+    }
